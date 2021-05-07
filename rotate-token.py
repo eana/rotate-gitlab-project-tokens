2e8f7a8fda3a01c9085b56b83852f94748d511e4
Jonas Eana <jonas@eana.ro>
2021-05-07 09:09:30 +0200

    Refactor the code

--- rotate-token.py
+++ rotate-token.py
@@ -1,29 +1,21 @@
+#!/usr/bin/env python
+
+import json
 import os
 from datetime import date, timedelta
 
 import requests
 
-api_v4_url = "https://gitlab.example.org/api/v4"
 
-# token details
-author = "Notes"
-expires_at = date.today() + timedelta(weeks=+1)
-env_var = "GITLAB_TOKEN"
-
-if "GITLAB_TOKEN" in os.environ:
-    header = {"PRIVATE-TOKEN": format(os.environ.get("GITLAB_TOKEN"))}
-else:
-    print("Please set the 'GITLAB_TOKEN' environment variable")
-    exit(1)
-
-
-def rotate_var(project_id, token):
-
-    # project_id = The ID of the current project. This ID is unique across all projects on the GitLab instance
-    # token = Project access token
+def rotate_gitlab_variable(**arguments):
+    api_v4_url = arguments["api_v4_url"]
+    header = arguments["header"]
+    project_id = arguments["project_id"]
+    env_var = arguments["env_var"]
+    token = arguments["token"]
 
     base_url = f"{api_v4_url}/projects/{project_id}/variables"
-    variables_url = base_url + "/" + env_var
+    variables_url = f"{base_url}/{env_var}"
 
     payload = {
         "key": env_var,
@@ -59,19 +51,20 @@ def rotate_var(project_id, token):
             raise SystemExit(e)
 
 
-def rotate_token(project_id):
-
-    # project_id = The ID of the current project. This ID is unique across all projects on the GitLab instance
+def rotate_gitlab_token(**arguments):
+    api_v4_url = arguments["api_v4_url"]
+    header = arguments["header"]
+    project_id = arguments["project_id"]
+    author = arguments["author"]
 
     base_url = f"{api_v4_url}/projects/{project_id}/access_tokens"
+    expires_at = date.today() + timedelta(weeks=+1)
 
-    payload = (
-        '{ "name": "'
-        + author
-        + '", "scopes": ["api"], "expires_at": "'
-        + str(expires_at)
-        + '" }'
-    )
+    payload = {
+        "name": f"{author}",
+        "scopes": ["api"],
+        "expires_at": f"{str(expires_at)}",
+    }
 
     # get the token id
     try:
@@ -100,21 +93,19 @@ def rotate_token(project_id):
 
     if "token_id" in locals() and isinstance(token_id, (int)):
         print(f"Token [name: '{author}', id: '{token_id}'] already exists, delete it")
+        delete_url = f"{base_url}/{str(token_id)}"
         try:
-            r = requests.delete(
-                base_url + "/" + str(token_id), headers=header, verify=False
-            )
+            r = requests.delete(delete_url, headers=header, verify=False)
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             raise SystemExit(e)
 
     print(f"Create new token [name: '{author}']")
     content_type = {"Content-Type": "application/json"}
+    headers = {**header, **content_type}
 
     try:
-        r = requests.post(
-            base_url, headers={**header, **content_type}, verify=False, data=payload
-        )
+        r = requests.post(base_url, headers=headers, verify=False, data=json.dumps(payload))
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         raise SystemExit(e)
@@ -122,10 +113,15 @@ def rotate_token(project_id):
     return r.json()["token"]
 
 
-def project_id(path_with_namespace):
+def get_project_id(**arguments):
+
+    api_v4_url = arguments["api_v4_url"]
+    header = arguments["header"]
+    path_with_namespace = arguments["path_with_namespace"]
+
     project = os.path.basename(path_with_namespace)
 
-    search_url = api_v4_url + "/search/?scope=projects&search=" + project
+    search_url = f"{api_v4_url}/search/?scope=projects&search={project}"
 
     try:
         r = requests.get(search_url, headers=header, verify=False)
@@ -147,6 +143,15 @@ def project_id(path_with_namespace):
 
 
 def main():
+    api_v4_url = "https://gitlab.example.org/api/v4"
+    author = "Notes"
+    env_var = "GITLAB_TOKEN"
+
+    try:
+        header = {"PRIVATE-TOKEN": format(os.environ["GITLAB_TOKEN"])}
+    except KeyError:
+        print("Please set the 'GITLAB_TOKEN' environment variable")
+        exit(1)
 
     for line in open("./projects.txt"):
         li = line.strip()
@@ -154,11 +159,23 @@ def main():
             path_with_namespace = line.rstrip()
 
             print(f"Processing: '{path_with_namespace}'")
-            id = project_id(path_with_namespace)
+            id = get_project_id(
+                api_v4_url=api_v4_url,
+                header=header,
+                path_with_namespace=path_with_namespace,
+            )
 
             if isinstance(id, (int)):
-                token = rotate_token(id)
-                rotate_var(id, token)
+                token = rotate_gitlab_token(
+                    api_v4_url=api_v4_url, header=header, author=author, project_id=id
+                )
+                rotate_gitlab_variable(
+                    api_v4_url=api_v4_url,
+                    header=header,
+                    env_var=env_var,
+                    token=token,
+                    project_id=id,
+                )
             else:
                 print(f"WARNING: '{path_with_namespace}' not found")
 
